59d811e6b7c8e71e8df3ff43de0a257842c6de65
qaelena181993 <85900418+qaelena181993@users.noreply.github.com>
2022-09-11 22:16:32 +0300

    Modification movieManagerTest

--- src/test/java/ru/netology/movieManagerTest.java
+++ src/test/java/ru/netology/movieManagerTest.java
@@ -26,7 +26,6 @@ class MovieManagerTest {
     void findAll() {
 
         String[] moviesMassive = new String[]{"Bloodshot", "LetsGo", "HostelBelgrade", "Gentlemen"};
-        manager.setMoviesPoster(moviesMassive);
         manager.findAll();
 
         String[] expected = new String[]{"Bloodshot", "LetsGo", "HostelBelgrade", "Gentlemen"};
@@ -39,7 +38,6 @@ class MovieManagerTest {
     void findLastManager() {
 
         String[] moviesMassive = new String[]{"Bloodshot", "LetsGo", "HostelBelgrade", "Gentlemen", "NumberOne"};
-        manager.setMoviesPoster(moviesMassive);
 
         String[] expected = new String[]{"NumberOne", "Gentlemen", "HostelBelgrade", "LetsGo", "Bloodshot"};
         String[] actual = manager.findLast();
@@ -51,7 +49,6 @@ class MovieManagerTest {
     void findLastManagerMoreThanTen() {
 
         String[] moviesMassive = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
-        manager.setMoviesPoster(moviesMassive);
 
         String[] expected = new String[]{"11", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
         String[] actual = manager.findLast();
@@ -64,9 +61,7 @@ class MovieManagerTest {
         movieManager manager = new movieManager(3);
 
         String[] moviesMassive = new String[]{"Bloodshot", "LetsGo", "HostelBelgrade", "Gentlemen", "NumberOne"};
-        manager.setMoviesPoster(moviesMassive);
-
-
+       
         String[] expected = new String[]{"NumberOne", "Gentlemen", "HostelBelgrade"};
         String[] actual = manager.findLast();
 
@@ -78,9 +73,7 @@ class MovieManagerTest {
         movieManager manager = new movieManager(11);
 
         String[] moviesMassive = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
-        manager.setMoviesPoster(moviesMassive);
-
-
+       
         String[] expected = new String[]{"11", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1"};
         String[] actual = manager.findLast();
 
