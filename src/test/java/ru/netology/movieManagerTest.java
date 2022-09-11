558152073cf991ef2154701e41dd774654609cd1
qaelena181993 <85900418+qaelena181993@users.noreply.github.com>
2022-09-11 22:05:08 +0300

    Modification movieManagerTest

--- src/test/java/ru/netology/movieManagerTest.java
+++ src/test/java/ru/netology/movieManagerTest.java
@@ -39,7 +39,6 @@ class movieManagerTest {
     void findLastManager() {
 
         String[] moviesMassive = new String[]{"Bloodshot", "LetsGo", "HostelBelgrade", "Gentlemen", "NumberOne"};
-        manager.setMoviesPoster(moviesMassive);
 
         String[] expected = new String[]{"NumberOne", "Gentlemen", "HostelBelgrade", "LetsGo", "Bloodshot"};
         String[] actual = manager.findLast();
@@ -51,8 +50,7 @@ class movieManagerTest {
     void findLastManagerMoreThanTen() {
 
         String[] moviesMassive = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
-        manager.setMoviesPoster(moviesMassive);
-
+     
         String[] expected = new String[]{"11", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
         String[] actual = manager.findLast();
 
@@ -64,8 +62,6 @@ class movieManagerTest {
         movieManager manager = new movieManager(3);
 
         String[] moviesMassive = new String[]{"Bloodshot", "LetsGo", "HostelBelgrade", "Gentlemen", "NumberOne"};
-        manager.setMoviesPoster(moviesMassive);
-
 
         String[] expected = new String[]{"NumberOne", "Gentlemen", "HostelBelgrade"};
         String[] actual = manager.findLast();
@@ -78,9 +74,7 @@ class movieManagerTest {
         movieManager manager = new movieManager(11);
 
         String[] moviesMassive = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
-        manager.setMoviesPoster(moviesMassive);
-
-
+    
         String[] expected = new String[]{"11", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1"};
         String[] actual = manager.findLast();
 
