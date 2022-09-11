53144c64cf79e14bd212fef5ac5f357d9e2053d9
qaelena181993 <85900418+qaelena181993@users.noreply.github.com>
2022-09-11 22:19:33 +0300

    Mod

--- src/test/java/ru/netology/movieManagerTest.java
+++ src/test/java/ru/netology/movieManagerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MovieManagerTest {
+class movieManagerTest {
     
     movieManager manager = new movieManager();
     
@@ -26,6 +26,7 @@ class MovieManagerTest {
     void findAll() {
 
         String[] moviesMassive = new String[]{"Bloodshot", "LetsGo", "HostelBelgrade", "Gentlemen"};
+        manager.setMoviesPoster(moviesMassive);
         manager.findAll();
 
         String[] expected = new String[]{"Bloodshot", "LetsGo", "HostelBelgrade", "Gentlemen"};
@@ -38,6 +39,7 @@ class MovieManagerTest {
     void findLastManager() {
 
         String[] moviesMassive = new String[]{"Bloodshot", "LetsGo", "HostelBelgrade", "Gentlemen", "NumberOne"};
+        manager.setMoviesPoster(moviesMassive);
 
         String[] expected = new String[]{"NumberOne", "Gentlemen", "HostelBelgrade", "LetsGo", "Bloodshot"};
         String[] actual = manager.findLast();
@@ -61,7 +63,9 @@ class MovieManagerTest {
         movieManager manager = new movieManager(3);
 
         String[] moviesMassive = new String[]{"Bloodshot", "LetsGo", "HostelBelgrade", "Gentlemen", "NumberOne"};
-       
+        manager.setMoviesPoster(moviesMassive);
+
+
         String[] expected = new String[]{"NumberOne", "Gentlemen", "HostelBelgrade"};
         String[] actual = manager.findLast();
 
@@ -73,7 +77,9 @@ class MovieManagerTest {
         movieManager manager = new movieManager(11);
 
         String[] moviesMassive = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"};
-       
+        manager.setMoviesPoster(moviesMassive);
+
+
         String[] expected = new String[]{"11", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1"};
         String[] actual = manager.findLast();
 
